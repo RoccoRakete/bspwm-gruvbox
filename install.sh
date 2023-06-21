1568eed1f1610085d207dca9f47f3f3b605c0b75
RoccoRakete <m.schaumann@mscloud.uk>
2023-06-21 21:36:16 +0200

    edited install script

--- install.sh
+++ install.sh
@@ -47,6 +47,63 @@ flatpak install com.spotify.Client
 mkdir -p ~/configs
 
 # clone repository
+cd ~
 git clone https://github.com/Martin1134/bspwm-gruvbox.git && cd bspwm-gruvbox
+cp -r * ~/configs
+rm -rf ~/bspwm-gruvbox
 
+# create hardlinks to .config directory
+rm -rf ~/.config/betterlockscreen 
+sudo ln -s ~/configs/betterlockscreen /home/martin/.config/
 
+rm -rf ~/.config/bspwm 
+sudo ln -s ~/configs/bspwm /home/martin/.config/
+
+rm -rf ~/.config/btop 
+sudo ln -s ~/configs/btop /home/martin/.config/
+
+rm -rf ~/.config/fish 
+sudo ln -s ~/configs/fish /home/martin/.config/
+
+rm -rf ~/.config/gtk-3.0 
+sudo ln -s ~/configs/gtk-3.0 /home/martin/.config/
+
+rm -rf ~/.config/htop 
+sudo ln -s ~/configs/htop /home/martin/.config/
+
+rm -rf ~/.config/kitty
+sudo ln -s ~/configs/kitty /home/martin/.config/
+
+rm -rf ~/.config/neofetch
+sudo ln -s ~/configs/neofetch /home/martin/.config/
+
+rm -rf ~/.config/picom 
+sudo ln -s ~/configs/picom /home/martin/.config/
+
+rm -rf ~/.config/polybar
+sudo ln -s ~/configs/polybar /home/martin/.config/
+
+rm -rf ~/.config/rofi
+sudo ln -s ~/configs/rofi /home/martin/.config/
+
+rm -rf ~/.config/sxhkd
+sudo ln -s ~/configs/sxhkd /home/martin/.config/
+
+rm -rf ~/.config/themes
+sudo ln -s ~/configs/themes /home/martin/.config/
+
+rm -rf ~/.config/Thunar
+sudo ln -s ~/configs/Thunar /home/martin/.config/
+
+rm -rf ~/.config/xfce4 
+sudo ln -s ~/configs/xfce4 /home/martin/.config/
+
+# make scripts executable
+chmod +X ~/configs/bspwm/autostart.sh
+chmod +X ~/configs/bspwm/bspwmrc
+chmod +X ~/configs/polybar/launch.sh
+chmod +X ~/configs/themes/gruvbox/bspwm/bspwmrc
+
+# restart bspwm / reload sxhkd config
+bspc wm -r
+pkill -USR1 -x sxhkd
