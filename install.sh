06c4c0e028efe3981ac7d7b54515552045595e39
RoccoRakete <m.schaumann@mscloud.uk>
2023-06-21 22:03:00 +0200

    edited install script

--- install.sh
+++ install.sh
@@ -2,42 +2,19 @@
 
 # install dependencies
 # pacman
-sudo pacman -S 
-feh \
-rofi \
-kitty \
-bspwm \
-polybar \
-thunar \
-thunar-archive-plugin \
-firefox \
-lightdm \
-nextcloud \
-kwallet \
-kwallet-pam \
-kwalletmanager \
-thunderbird \
-nextcloud-client \
-flatpak \
-picom \
-pavucontrol \
-sxhkd \
-fish \
-htop \
-btop \
-git \
+sudo pacman -S feh rofi kitty bspwm polybar thunar thunar-archive-plugin firefox lightdm nextcloud kwallet kwallet-pam kwalletmanager thunderbird nextcloud-client flatpak picom pavucontrol sxhkd fish htop btop git  
 
 # install yay as aur helper
 pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 # yay (AUR)
 yay -S 
-themix-full-git \
-lxappearance \
-nerd-fonts-git \
-pywal \
-thunar-shares-plugin \
-visual-studio-code-bin \
+themix-full-git  
+lxappearance  
+nerd-fonts-git  
+pywal  
+thunar-shares-plugin  
+visual-studio-code-bin  
 
 # flatpak
 flatpak install com.spotify.Client
