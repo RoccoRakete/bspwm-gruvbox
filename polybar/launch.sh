01d5eda4e0f910e1406264bb948f4506b763e63a
RoccoRakete <m.schaumann@mscloud.uk>
2023-07-08 11:23:50 +0200

    1

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# Terminate already running bar instances
+# Terminate already running bar instances1
 # If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
